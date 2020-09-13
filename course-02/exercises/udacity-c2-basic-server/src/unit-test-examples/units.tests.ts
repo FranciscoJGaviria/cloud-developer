8961943e324925f85f1e4458bfe26e37931c59a2
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2020-09-13 14:26:35 -0500

    feat(exercise-one): basic express server and unit test

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, multiply, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -35,6 +35,60 @@ describe('divide', () => {
 
 });
 
+describe('multiply', () => {
+  it ('should multiply 20 by 3', () => {
+    const a = 20;
+    const b = 3;
+
+    const result = multiply(a, b);
+
+    expect(result).to.equal(60);
+  });
+
+  it ('should return zero', () => { 
+    const a = 0;
+    const b = 1012;
+
+    const result = multiply(a, b);
+
+    expect(result).to.equal(0)
+  });
+})
+
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('Concat', () => {
+  it('Should concat two strings', () => {
+    const a = 'Pacho '
+    const b = 'Gaviria'
+
+    const result = concat(a, b)
+
+    expect(result).to.equal('Pacho Gaviria')
+  })
+
+  it('Should fail with the first param as null', () => {
+    const a: string = null
+    const b = 'Gaviria'
+    
+    expect(() => concat(a, b), 'Both parameters are required')
+  })
+
+  it('Should fail with the second param as undefined', () => {
+    const a = 'Pacho '
+    const b: string = undefined
+
+    expect(() => concat(a, b), 'Both parameters are required')
+  })
+
+
+  it('Should concat two empty string', () => {
+    const a = ''
+    const b = ''
+
+    const result = concat(a, b)
+
+    expect(result).to.equal('')
+  })
+})
